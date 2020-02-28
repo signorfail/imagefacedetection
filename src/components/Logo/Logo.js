861636f4428ddd835599f77fa5931d206910c51f
Eugene Yap <signorfail@gmail.com>
2020-02-28 15:45:34 +0900

    Re-did logo size, position & padding

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,9 +6,9 @@ import facialRecognitionLogo from './facialRecognitionLogo.png';
 const Logo = () => {
 	return (
 		<div className='ma4 mt0'>
-			<Tilt className="Tilt br2 shadow-2" options={{ max : 25 }} style={{ height: 250, width: 250 }} >
- 				<div className="Tilt-inner pa3">
- 					<img style={{paddingTop:'5px'}} alt='logo' src={facialRecognitionLogo}/>
+			<Tilt className="Tilt br2 shadow-2" options={{ max : 25 }} style={{ height: 100, width: 100 }} >
+ 				<div className="Tilt-inner pa1">
+ 					<img style={{paddingTop:'1px'}} alt='logo' src={facialRecognitionLogo}/>
  				</div>
 			</Tilt>
 		</div>
