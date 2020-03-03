8b7441a237381ac7516dfabbd44ccd534fb7443f
Eugene Yap <signorfail@gmail.com>
2020-03-03 13:40:34 +0900

    Added Face Detection API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation.js';
 import Logo from './components/Logo/Logo.js';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition.js';
 import Rank from './components/Rank/Rank.js';
 import Particles from 'react-particles-js';
 import './App.css';
+
+const app = new Clarifai.App({
+ apiKey: 'dc0dec044ee247be9d76934e0267aacc'
+});
 
 const particlesOptions = {
   particles: {
@@ -21,19 +27,49 @@ const particlesOptions = {
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className='particles' params={particlesOptions} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-     {//       <FaceRecognition />
-     //       Image display area with face recognition
-   }
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state={
+      input: '',
+      imageUrl:'',
+    }
+  }
+
+  onInputChange=(event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit=() => {
+    this.setState({imageUrl: this.state.input});
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
+      function(response) {
+        // do something with response
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+      },
+      function(err) {
+        // there was an error
+      }
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Particles className='particles' params={particlesOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit} 
+          />
+       <FaceRecognition imageUrl={this.state.imageUrl} /> {
+        // To display image sent to Clarifai API using app.models.predict
+      }
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
 	return (
 		<div>
 			<p className='f3'>
 				{'Face Detection in Images Using Machine Learning.'}
 			</p>
-			<div classname='center'>
-				<div classname='center pa4 br3 shadow-5'>
-					<input className='f4 pa2 w-30 center' type='text' />
-					<button className='w-20 grow f4 link ph3 pv2 ml1 dib'>Run</button>
+			<div className='center'>
+				<div className='center pa4 br3 shadow-5'>
+					<input className='f4 pa2 w-30 center' type='text' placeholder='Enter image link.' onChange={onInputChange} />
+					<button 
+						className='w-20 grow f4 link ph3 pv2 ml1 dib'
+						onClick={onButtonSubmit}
+						>Run</button>
 				</div>
 			</div>
 		</div>
